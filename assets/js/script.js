61113a622464c903f87ed7e5c6a4db121f7c68b5
Fran804 <franciscovillalobos804@gmail.com>
2023-02-24 16:54:35 -0600

    finished navbar

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// ====== NAVBAR ======
+
+var iconBars = document.querySelector('.icon-bars');
+var navBar = document.querySelector('.nav-bar');
+
+iconBars.addEventListener('click', () => {
+  navBar.classList.toggle("active")
+})
+
+// ====== END NAVBAR ======
