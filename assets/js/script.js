281eaa83c37f1f0ad5024d5ec1f04686821be1bd
Fran804 <franciscovillalobos804@gmail.com>
2023-06-05 18:41:46 -0600

    Start Services

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,23 @@ iconBars.addEventListener('click', () => {
 })
 
 // ====== END NAVBAR ======
+
+window.addEventListener('scroll', function(){
+  var card = document.querySelectorAll('.fade-up')
+  
+  for (var i = 0; i < card.length; i++) {
+    var distance = card[i].getBoundingClientRect().top;
+    var height = window.innerHeight/1.3;
+    
+    card[i].classList.add('transform-up');
+
+    if(distance <= height){
+      card[i].classList.add('show-card')
+      return;
+    }
+    else {
+      card[i].classList.remove('show-card')
+    }
+    
+  }
+})
