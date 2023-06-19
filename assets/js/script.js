b4f0e40552c1dd1f1a78e345fb6c19c9f08ac754
Fran804 <franciscovillalobos804@gmail.com>
2023-06-19 11:39:30 -0600

    finish contact

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 // ====== NAVBAR ======
-
 var iconBars = document.querySelector('.icon-bars');
 var navBar = document.querySelector('.nav-bar');
 
@@ -8,3 +7,23 @@ iconBars.addEventListener('click', () => {
 })
 
 // ====== END NAVBAR ======
+
+// ====== CONTACT ======
+
+var btnContactWhats = document.querySelector('.contact-whats');
+var btnContactEmail = document.querySelector('.contact-email');
+var btnContactInsta = document.querySelector('.contact-insta');
+
+btnContactWhats.addEventListener('click', () => {
+  window.open("https://wa.me/9995445986");
+});
+
+btnContactEmail.addEventListener('click', () => {
+  window.open("https://mail.google.com/mail/u/0/#inbox?compose=DmwnWrRttNfvfmJflPFBNBTlWxzqDdVCSWZKhPqlDsLfwkTnWvspwKHZJLSDbJxxwPGFQJNljhlv");
+});
+
+btnContactInsta.addEventListener('click', () => {
+  window.open("https://www.instagram.com/beautysalonale_/?hl=es");
+});
+
+// ====== END CONTACT ======
