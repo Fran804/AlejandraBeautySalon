0970fc1e921045b2ea0faaf7f9a56d80d3f39d75
Fran804 <franciscovillalobos804@gmail.com>
2023-06-20 23:43:12 -0600

    adding finals touchs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,46 @@
 // ====== NAVBAR ======
 var iconBars = document.querySelector('.icon-bars');
 var navBar = document.querySelector('.nav-bar');
+var homeNavbar = document.querySelector('.homeNav');
+var servicesNavbar = document.querySelector('.servicesNav');
+var galeryNavbar = document.querySelector('.galeryNav');
+var reviewsNavbar = document.querySelector('.reviewsNav');
+var contactNavbar = document.querySelector('.contactNav');
 
 iconBars.addEventListener('click', () => {
   navBar.classList.toggle("active")
-})
+});
 
+homeNavbar.addEventListener('click', () => {
+  navBar.classList.remove("active");
+});
+
+servicesNavbar.addEventListener('click', () => {
+  navBar.classList.remove("active");
+});
+
+galeryNavbar.addEventListener('click', () => {
+  navBar.classList.remove("active");
+});
+
+reviewsNavbar.addEventListener('click', () => {
+  navBar.classList.remove("active");
+});
+
+contactNavbar.addEventListener('click', () => {
+  navBar.classList.remove("active");
+});
 // ====== END NAVBAR ======
 
-// ====== CONTACT ======
+// ====== HOME ======
+var btnHome = document.querySelector('.btn-home');
 
+btnHome.addEventListener('click', () => {
+  window.open("https://wa.me/9995445986");
+})
+// ====== END HOME ======
+
+// ====== CONTACT ======
 var btnContactWhats = document.querySelector('.contact-whats');
 var btnContactEmail = document.querySelector('.contact-email');
 var btnContactInsta = document.querySelector('.contact-insta');
@@ -25,5 +56,4 @@ btnContactEmail.addEventListener('click', () => {
 btnContactInsta.addEventListener('click', () => {
   window.open("https://www.instagram.com/beautysalonale_/?hl=es");
 });
-
 // ====== END CONTACT ======
